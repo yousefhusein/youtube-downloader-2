67ce2fc7ae70a4efbffcbe07d983965725d6c39b
yousefhusein <1yousefakrem@gmail.com>
2025-02-26 17:30:25 +0300

    updates

--- server.js
+++ server.js
@@ -5,47 +5,11 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 const path = require('path')
 const cors = require('cors')
-const os = require('node:os')
+const updateCookies = require('./updateCookies')
 const app = express()
 
-const cookiesPath = path.join(__dirname, './cookies.json')
-let isUpdating = false
-
-setInterval(async () => {
-  if (!isUpdating) {
-    isUpdating = true
-    try {
-      let executablePath
-
-      if (os.type().toLowerCase().includes('Windows')) {
-        console.log(`Windows detected`)
-        executablePath = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
-      } else {
-        executablePath = '/usr/bin/google-chrome'
-      }
-
-      console.log('Launching puppeteer')
-      const browser = await puppeteer.launch({
-          browser: 'chrome',
-          executablePath,
-        }),
-        page = await browser.newPage(),
-        youtube = await page.goto('https://youtube.com'),
-        cookies = await browser.cookies()
-
-      console.log(`Cookies are updated!`)
-
-      fs.writeFileSync(cookiesPath, JSON.stringify(cookies, null, 4))
-
-      await browser.close()
-    } catch (error) {
-      console.error(error)
-      console.error("Couldn't update cookies")
-    } finally {
-      isUpdating = false
-    }
-  }
-}, 1000 * 5)
+updateCookies()
+setInterval(updateCookies, 1000 * 60 * 5)
 
 app.use(
   cors({
@@ -53,6 +17,7 @@ app.use(
     exposedHeaders: ['x-video-title'],
   }),
 )
+
 app.use('/api/download/youtube', require('./routes/youtube'))
 
 app.listen(8080, () => {
---
+++ updateCookies.js
@@ -0,0 +1,42 @@
+const fs = require('node:fs')
+const path = require('node:path')
+const puppeteer = require('puppeteer')
+const os = require('node:os')
+const cookiesPath = path.join(__dirname, './cookies.json')
+let isUpdating = false
+
+module.exports = async function updateCookies() {
+  if (!isUpdating) {
+    isUpdating = true
+    try {
+      let executablePath
+
+      if (os.type().toLowerCase().includes('Windows')) {
+        console.log(`Windows detected`)
+        executablePath = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+      } else {
+        executablePath = '/usr/bin/google-chrome'
+      }
+
+      console.log('Launching puppeteer')
+      const browser = await puppeteer.launch({
+          browser: 'chrome',
+          executablePath,
+        }),
+        page = await browser.newPage(),
+        youtube = await page.goto('https://youtube.com'),
+        cookies = await browser.cookies()
+
+      console.log(`Cookies are updated!`)
+
+      fs.writeFileSync(cookiesPath, JSON.stringify(cookies, null, 4))
+
+      await browser.close()
+    } catch (error) {
+      console.error(error)
+      console.error("Couldn't update cookies")
+    } finally {
+      isUpdating = false
+    }
+  }
+}
